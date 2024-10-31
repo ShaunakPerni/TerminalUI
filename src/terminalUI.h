1b2c514ddf6b548d2dd38280c7a8c94533d526cc
Shaunak Perni <134259584+ShaunakPerni@users.noreply.github.com>
2024-10-31 19:19:59 +0530

    Added Doxygen Comments

--- src/terminalUI.h
+++ src/terminalUI.h
@@ -13,12 +13,24 @@ namespace Package_TerminalUI
 {
     namespace formattedOutputs
     {
+        /**
+         * @brief Generate a title bar
+         * @param text The text to be displayed in the title bar
+         * @param color The color of the title bar
+         */
         void generateTitleBar(std::string text, std::string color)
         {
             terminalFunctions::clearScreen();
             std::cout << color << text << "\n" << terminalFormatingColorcodes::ANSI_RESET;
         }
 
+        /**
+         * @brief Print headers for a menu
+         * @param selection The selected header
+         * @param Headers A vector of header strings
+         * @param colorActive The color for the selected header
+         * @param colorInactive The color for the unselected headers
+         */
         void printHeaders(int selection, const std::vector<std::string> &Headers, std::string colorActive, std::string colorInactive)
         {
             for (int nHeader = 0; nHeader < Headers.size(); nHeader++)
@@ -30,6 +42,11 @@ namespace Package_TerminalUI
             std::cout << std::endl;
         }
 
+        /**
+         * @brief Print a menu with options
+         * @param directionVertical If true the menu is printed vertically, otherwise horizontally
+         * @param options A vector of strings representing the menu options
+         */
         void optionsMenu(bool directionVertical, std::vector<std::string> options)
         {
             std::string endline = "";
@@ -41,6 +58,12 @@ namespace Package_TerminalUI
             std::cout << std::endl;
         }
 
+        /**
+         * @brief Print a menu with options
+         * @param directionVertical If true the menu is printed vertically, otherwise horizontally
+         * @param options A vector of strings representing the menu options
+         * @param color The color of the menu
+         */
         void optionsMenu(bool directionVertical, std::vector<std::string> options, std::string color)
         {
             std::string endline = "";
@@ -52,27 +75,47 @@ namespace Package_TerminalUI
             std::cout << std::endl;
         }
 
+        /**
+         * @brief Add a marker in the output
+         * @param MarkerColor The color of the marker
+         */
         void addMarker(std::string MarkerColor)
         {
             std::cout << MarkerColor;
         }
 
+        /**
+         * @brief Reset the marker in the output
+         */
         void resetMarker()
         {
             std::cout << terminalFormatingColorcodes::ANSI_RESET;
         }
 
+        /**
+         * @brief Highlight a string in the output
+         * @param text The string to be highlighted
+         * @param HighlightColor The color of the highlight
+         */
         void highlightText(std::string text, std::string HighlightColor)
         {
             std::cout << HighlightColor << text << terminalFormatingColorcodes::ANSI_RESET;
         }
 
+        /**
+         * @brief A struct to hold a string and its associated color
+         */
         struct stringWColor
         {
             std::string optionString;
             std::string color;
         };
 
+        /**
+         * @brief Print a menu with options and custom colors
+         * @param directionVertical If true the menu is printed vertically, otherwise horizontally
+         * @param optionsStruct A vector of stringWColor structs representing the menu options and their colors
+         */
         void fancyOptionsMenu(bool directionVertical, std::vector<stringWColor> optionsStruct)
         {
             std::string endline = "";
@@ -85,6 +128,13 @@ namespace Package_TerminalUI
             std::cout << std::endl;
         }
 
+        /**
+         * @brief Print a value with a binary color threshold
+         * @param data The value to be printed
+         * @param colorAbove The color to use if the value is above the midpoint
+         * @param colorBelow The color to use if the value is below the midpoint
+         * @param midPoint The midpoint to be used for the threshold
+         */
         template <typename ratedDataType>
         void binaryColorThresholdFormatedValue(ratedDataType data, std::string colorAbove, std::string colorBelow, ratedDataType midPoint)
         {
@@ -92,6 +142,13 @@ namespace Package_TerminalUI
             std::cout << data << terminalFormatingColorcodes::ANSI_RESET;
         }
 
+        /**
+         * @brief Print a value with an alternating color
+         * @param printValue The value to be printed
+         * @param alternator A pointer to a boolean that is flipped each time the function is called
+         * @param Color1 The first color
+         * @param Color2 The second color
+         */
         void binaryColorAlternateFormatedValue(std::string printValue, bool *alternator, std::string Color1, std::string Color2)
         {
             *alternator ? addMarker(Color1) : addMarker(Color2);
@@ -102,6 +159,12 @@ namespace Package_TerminalUI
     }
     namespace formattedPrompts
     {
+        /**
+         * @brief Prompt the user for input with a given symbol
+         * @tparam returnData The type of the input data to be returned
+         * @param promptSymbol The symbol to display as a prompt
+         * @return The user input converted to returnData type
+         */
         template <typename returnData>
         returnData customPrompt(std::string promptSymbol)
         {
@@ -111,16 +174,35 @@ namespace Package_TerminalUI
             return tempReturnData;
         }
 
+        /**
+         * @brief Prompt the user for input with a given symbol and color
+         * @tparam returnData The type of the input data to be returned
+         * @param promptSymbol The symbol to display as a prompt
+         * @param color The color of the prompt symbol
+         * @return The user input converted to returnData type
+         */
         template <typename returnData>
         returnData customPrompt(std::string promptSymbol, std::string color)
         {
             returnData tempReturnData;
             std::cout << color << promptSymbol;
             std::cin >> tempReturnData;
-            std::cout<< terminalFormatingColorcodes::ANSI_RESET;
+            std::cout << terminalFormatingColorcodes::ANSI_RESET;
             return tempReturnData;
         }
 
+        /**
+         * @brief Prompt the user for two inputs with messages and colors
+         * @tparam returnDataMember0 The type of the first input data
+         * @tparam returnDataMember1 The type of the second input data
+         * @param formMessage The message to display before prompting
+         * @param promptSymbol The symbol to display as a prompt
+         * @param input0Message The message for the first input prompt
+         * @param input1Message The message for the second input prompt
+         * @param input0Color The color of the first input prompt
+         * @param input1Color The color of the second input prompt
+         * @return A pair containing the first and second user inputs
+         */
         template <typename returnDataMember0, typename returnDataMember1>
         auto customPromptFormDualInputs(std::string formMessage = "", std::string promptSymbol = ">:", std::string input0Message = "", std::string input1Message = "", std::string input0Color = "", std::string input1Color = "")
         {
